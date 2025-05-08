8b0304aea9d67bf8d72f5b7de93df2680536045f
himadree-chaudhury <himchy2001@gmail.com>
2025-05-08 16:31:33 +0600

    solve question2

--- problems.ts
+++ problems.ts
@@ -1,4 +1,5 @@
 {
+    // *Problem 1
   function formatString(input: string, toUpper?: boolean): string {
     if (toUpper === undefined || toUpper === true) {
       return input.toUpperCase();
@@ -7,5 +8,21 @@
     }
   }
 
-  console.log(formatString("hello"));
+  //   console.log(formatString("hello"));
+  //   console.log(formatString("hello",true));
+  //   console.log(formatString("hello",false));
+
+  // *Problem 2
+  function filterByRating(
+    items: { title: string; rating: number }[]
+  ): { title: string; rating: number }[] {
+    return items.filter((item) => item.rating >= 4);
+  }
+
+  const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 },
+  ];
+  // console.log(filterByRating(books));
 }
