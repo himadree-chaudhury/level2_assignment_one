8f65d53b3bdd47c68ecab2fef277ada130f43b15
himadree-chaudhury <himchy2001@gmail.com>
2025-05-08 23:32:32 +0600

    check

--- problems.ts
+++ problems.ts
@@ -76,7 +76,7 @@
   async function squareAsync(n: number): Promise<number> {
     return new Promise((resolve, reject) => {
       if (n < 0) {
-        reject(new Error("Negative numbers are not allowed"));
+        reject(new Error("Negative number not allowed"));
       }
       setTimeout(() => {
         resolve(n * n);
