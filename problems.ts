522a057aab4dc3630cb304491df079e0b47aee70
himadree-chaudhury <himchy2001@gmail.com>
2025-05-08 16:24:27 +0600

    solve question1

---
+++ problems.ts
@@ -0,0 +1,11 @@
+{
+  function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === undefined || toUpper === true) {
+      return input.toUpperCase();
+    } else {
+      return input.toLowerCase();
+    }
+  }
+
+  console.log(formatString("hello"));
+}
